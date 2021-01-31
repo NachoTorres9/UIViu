adf951df1ce99cb9572ce49f918ab49a19626ca3
Jeronimo Moya Ballano <jeronimomoya17@gmail.com>
2021-01-31 11:59:27 +0100

    Estructura home

---
+++ js/Entrega3.js
@@ -0,0 +1,19 @@
+﻿function inicializa(){
+    $("#margen").css("height",screen.height/6+"px")
+    
+}
+
+function iniciarSesion(){
+    if ($("#clave").val().length <6){
+        $("#error").show();
+        
+    }
+        
+    else {
+        var usuario = $("#txtUsuario").val();
+        $("#inicioSesion").hide()
+        $("#home").show();
+        $("body").css("background-color","white" )
+        $("#usuario").text("Bienvenido: "+usuario);
+    }
+}
